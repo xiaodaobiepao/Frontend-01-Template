df9cecee8f722f94ed0ef400ea7d2319c0c3c59b
xiaodaobiepao <1310851368@qq.com>
2020-05-14 00:45:14 +0800

    format

--- week05/client.js
+++ week05/client.js
@@ -74,23 +74,23 @@ class HttpRequest {
 
 class ResponseParser {
   constructor () {
-    this.WAITING_STATUS_LINE = 0;
-    this.WAITING_STATUS_LINE_END = 1;
-    this.WAITING_HEADER_NAME = 2;
-    this.WAITING_HEADER_NAME_END = 3;
-    this.WAITING_HEADER_SPACE = 4;
-    this.WAITING_HEADER_VALUE = 5;
-    this.WAITING_HEADER_LINE_END = 6;
-    this.WAITING_HEADER_BLOCK_END = 7;
-    this.WAITING_BODY = 8;
+    this.WAITING_STATUS_LINE = 0
+    this.WAITING_STATUS_LINE_END = 1
+    this.WAITING_HEADER_NAME = 2
+    this.WAITING_HEADER_NAME_END = 3
+    this.WAITING_HEADER_SPACE = 4
+    this.WAITING_HEADER_VALUE = 5
+    this.WAITING_HEADER_LINE_END = 6
+    this.WAITING_HEADER_BLOCK_END = 7
+    this.WAITING_BODY = 8
 
-    this.current = this.WAITING_STATUS_LINE;
-    this.statusLine = '';
-    this.headers = {};
-    this.headerName = '';
-    this.headerValue = '';
+    this.current = this.WAITING_STATUS_LINE
+    this.statusLine = ''
+    this.headers = {}
+    this.headerName = ''
+    this.headerValue = ''
 
-    this.bodyParser = null;
+    this.bodyParser = null
   }
 
   get isFinished () {
@@ -98,7 +98,7 @@ class ResponseParser {
   }
 
   get response () {
-    this.statusLine.match(/^HTTP\/1\.1 ([1-5]\d{2}) (\w+)/);
+    this.statusLine.match(/^HTTP\/1\.1 ([1-5]\d{2}) (\w+)/)
     return {
       statusCode: RegExp.$1,
       statusTxet: RegExp.$2,
@@ -109,37 +109,37 @@ class ResponseParser {
 
   receive (string) {
     for (let i = 0; i < string.length; i++) {
-      this.receiveChar(string.charAt(i));
+      this.receiveChar(string.charAt(i))
     }
   }
 
   receiveChar (char) {
     if (this.current === this.WAITING_STATUS_LINE) {
       if (char === '\r') {
-        this.current = this.WAITING_STATUS_LINE_END;
+        this.current = this.WAITING_STATUS_LINE_END
       } else {
-        this.statusLine += char;
+        this.statusLine += char
       }
     } else if (this.current === this.WAITING_STATUS_LINE_END) {
-      this.current = this.WAITING_HEADER_NAME;
+      this.current = this.WAITING_HEADER_NAME
     } else if (this.current === this.WAITING_HEADER_NAME) {
       if (char === '\r') {
         this.current = this.WAITING_HEADER_BLOCK_END
       } else if (char === ':') {
         this.current = this.WAITING_HEADER_SPACE;
       } else {
-        this.headerName += char;
+        this.headerName += char
       }
     } else if (this.current === this.WAITING_HEADER_SPACE) {
       this.current = this.WAITING_HEADER_VALUE
     } else if (this.current === this.WAITING_HEADER_VALUE) {
       if (char === '\r') {
-        this.current = this.WAITING_HEADER_LINE_END;
-        this.headers[this.headerName] = this.headerValue;
-        this.headerName = '';
-        this.headerValue = '';
+        this.current = this.WAITING_HEADER_LINE_END
+        this.headers[this.headerName] = this.headerValue
+        this.headerName = ''
+        this.headerValue = ''
       } else {
-        this.headerValue += char;
+        this.headerValue += char
       }
     } else if (this.current === this.WAITING_HEADER_LINE_END) {
       this.current = this.WAITING_HEADER_NAME
@@ -156,14 +156,14 @@ class ResponseParser {
 
 class ChunkedBodyParser {
   constructor () {
-    this.READING_LENGTH_FIRSR_CHAR = 0;
-    this.READING_LENGTH = 1;
-    this.READING_LENGTH_END = 2;
-    this.READING_CHUNK = 3;
-    this.READING_CHUNK_END = 4;
-    this.BODY_BLOCK_END = 5;
+    this.READING_LENGTH_FIRSR_CHAR = 0
+    this.READING_LENGTH = 1
+    this.READING_LENGTH_END = 2
+    this.READING_CHUNK = 3
+    this.READING_CHUNK_END = 4
+    this.BODY_BLOCK_END = 5
 
-    this.current = this.READING_LENGTH_FIRSR_CHAR;
+    this.current = this.READING_LENGTH_FIRSR_CHAR
     this.content = []
     this.chunkLength = 0
   }
@@ -173,31 +173,31 @@ class ChunkedBodyParser {
   }
 
   receiveChar (char) {
-    if (this.current === this.READING_LENGTH_FIRSR_CHAR) { // Length的第一个字符是单独一个状态
-      if (char === '0') { // Length的第一个字符是'0'的话就是终止块
-        this.current = this.BODY_BLOCK_END;
+    if (this.current === this.READING_LENGTH_FIRSR_CHAR) {
+      if (char === '0') {
+        this.current = this.BODY_BLOCK_END
       } else {
-        this.chunkLength += Number(`0x${char}`); // chunk-length在包体是16进制
-        this.current = this.READING_LENGTH;
+        this.chunkLength += Number(`0x${char}`)
+        this.current = this.READING_LENGTH
       }
     } else if (this.current === this.READING_LENGTH) {
       if (char === '\r') {
-        this.current = this.READING_LENGTH_END;
+        this.current = this.READING_LENGTH_END
       } else {
-        this.chunkLength = this.chunkLength * 16 + Number(`0x${char}`);
+        this.chunkLength = this.chunkLength * 16 + Number(`0x${char}`)
       }
     } else if (this.current === this.READING_LENGTH_END) {
-      this.current = this.READING_CHUNK;
+      this.current = this.READING_CHUNK
     } else if (this.current === this.READING_CHUNK) {
       if (char === '\r') {
         this.current = this.READING_CHUNK_END
         this.chunkLength = 0
       } else if (this.chunkLength > 0) {
-        this.content.push(char);
-        this.chunkLength -= 1;
+        this.content.push(char)
+        this.chunkLength -= 1
       }
     } else if (this.current === this.READING_CHUNK_END) {
-      this.current = this.READING_LENGTH_FIRSR_CHAR;
+      this.current = this.READING_LENGTH_FIRSR_CHAR
     }
   }
 }
