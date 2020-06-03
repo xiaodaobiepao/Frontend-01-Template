6c88cb471048eaf150c2023b4e1a2347f046c9c3
xiaodaobiepao <1310851368@qq.com>
2020-06-04 02:08:22 +0800

    wanzhengban

--- week08/match.js
+++ week08/match.js
@@ -1,6 +1,8 @@
 function match(element, selector) {
   // const parents = element.parentElement
-  const selectMatchs = selector.match(/([a-z\d\-\.#\[\]:'"=]+[\s>~+]*)/g).map(m => m.replace(/\s/, '')).reverse()
+  selector = selector.toLowerCase()
+  // 利用正则将串行选择器拆分成一个个单个的选择器，方便遍历与dom节点匹配
+  const selectMatchs = selector.match(/[a-z\d\-\.#]+(\[[a-z_\-|^*$'"= ]+\])*[a-z\d\-\.#:]*[\s>~+]*/g).map(m => m.trim()).reverse()
   console.log(selectMatchs)
   console.log('first', isSameNode(element, selectMatchs[0]))
   if (!isSameNode(element, selectMatchs[0])) {
@@ -10,19 +12,22 @@ function match(element, selector) {
   let node = element
   let i = 1, length = selectMatchs.length - 1
   while (i < length) {
-    const cur = selectMatchs[i]
+    let cur = selectMatchs[i]
     if (cur.endsWith('>')) {
       // 子孙
       node = node.parentElement
+      cur = cur.slice(0, cur.length - 1).trim()
       if (!isSameNode(node, cur)) {
         return false
       }
     } else if (cur.endsWith('+')) {
+      cur = curcur.slice(0, cur.length - 1).trim()
       if (!element.previousElementSibling || !isSameNode(element.previousElementSibling, cur)) {
         return false
       }
     } else if (cur.endsWith('~')) {
       let prev = element.previousElementSibling
+      cur = cur.slice(0, cur.length - 1).trim()
       while (prev) {
         if (isSameNode(prev, cur)) {
           break
@@ -54,12 +59,16 @@ function match(element, selector) {
 
 function isSameNode (node, selectorStr) {
   // 判断当前节点是否是当前选择器
+  const tagName = /[a-z]/.test(selectorStr[0]) ? selectorStr.match(/([a-z]+)/)[1] : '' // 标签必须在第一位
   const id = (selectorStr.match(/#[a-z_\d\-]+/) || [])[1] // 获取第一个匹配的id
   const classList = (selectorStr.match(/\.[a-z_\d\-_]+/g) || []).map(m => m.slice(1))
-  const attrList = (selectorStr.match(/\[[a-z_\-\d='"~|]+\]/g) || []).map(m => m.slice(1, m.length - 1))
-  const pseudoClasses = (selectorStr.match(/:[a-z\-]+/g) || []).map(m => m.slice(1))
-  const pseudoElements = (selectorStr.match(/::[a-z\-]+/g) || []).map(m => m.slice(2))
+  const attrList = (selectorStr.match(/\[[a-z_\-\d='"~|^*$ ]+\]/g) || []).map(m => m.slice(1, m.length - 1))
+  const pseudoClasses = (selectorStr.match(/:[a-z\-]+/g) || []).map(m => m.slice(1))  // 不考虑伪类
+  const pseudoElements = (selectorStr.match(/::[a-z\-]+/g) || []).map(m => m.slice(2)) // 也不考虑伪元素
 
+  if (tagName && node.tagName.toLowerCase() !== tagName) {
+    return false
+  }
   if (id && node.getAttribute('id') !== id) {
     return false
   }
@@ -72,6 +81,22 @@ function isSameNode (node, selectorStr) {
   }
   if (attrList && attrList.length) {
     // 判断属性是否一致
+    for (let i = 0; i < attrList.length; ++i) {
+      const curAttr = attrList[i]
+      const matchs = curAttr.match(/([a-z_\-\d]+)([~|]*=)*(?:"([a-z_\-\d ]*)")*/)
+      const name = matchs[1]
+      const symbole = matchs[2]
+      const value = matchs[3]
+      if (name && !symbole && !node.hasAttribute(name)) {
+        return false
+      } else if (symbole === '=' && node.getAttribute(name) !== value) {
+        return false
+      } else if (symbole === '~=' && !node.getAttribute(name).split(' ').includes(value)) {
+        return false
+      } else if (symbole === '|=' && !node.getAttribute(name).startsWith(value) && !node.getAttribute(name).startsWith(value + '-')) {
+        return false
+      }
+    }
   }
   return true
 }
