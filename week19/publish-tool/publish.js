6140551f56be033c0c3d00c6fe28f85043416ca3
xiaodaobiepao <1310851368@qq.com>
2020-08-29 22:09:36 +0800

    complete

--- week19/publish-tool/publish.js
+++ week19/publish-tool/publish.js
@@ -1,9 +1,68 @@
 const http = require('http')
 const querystring = require('querystring')
+const fs = require('fs')
+const { fstat } = require('fs')
+const archiver = require('archiver')
 
-const postData = querystring.stringify({
-  'msg': 'Hello World zidingyineir'
-})
+
+// let filename = './cat.jpg'
+let packname = './package'
+
+// fs.stat(filename, (error, stat) => {
+  // console.log(stat.size)
+  const options = {
+    host: 'localhost',
+    port: 3000,
+    path: '/?filename=package.zip',
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/octet-stream'
+    }
+  }
+
+  const archive = archiver('zip', {
+    zlib: { level: 9 }
+  })
+
+  archive.directory(packname, false)
+
+  // archive.pipe(fs.createWriteStream('./package.zip'))
+
+  archive.finalize()
+
+  const req = http.request(options, res => {
+    console.log(`STATUS ${res.statusCodd}`)
+    console.log(`HEADERS: ${JSON.stringify(res.headers)}`)
+    // res.setEncoding('utf8')
+    // res.on('data', chunk => {
+    //   console.log(`BODY: ${chunk}`)
+    // })
+    // res.on('end', () => {
+    //   console.log('No more data in response')
+    // })
+  })
+  req.on('error', err => {
+    console.error(`problem with request: ${err.message}`)
+  })
+
+  archive.pipe(req)
+
+  archive.on('end', () => {
+    console.log('end')
+    req.end()
+  })
+
+  // let readStream = fs.createReadStream('./cat.jpg')
+  // readStream.pipe(req)
+
+  // readStream.on('end', () => {
+  //   req.end()
+  // })
+// })
+
+// const postData = querystring.stringify({
+//   'msg': 'Hello World zidingyineir'
+// })
 
 // const options = {
 //   host: 'localhost',
@@ -11,32 +70,3 @@ const postData = querystring.stringify({
 //   path: '/?filename=x.html',
 //   method: 'GET'
 // }
-
-const options = {
-  host: 'localhost',
-  port: 3000,
-  path: '/?filename=test3.html',
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/octet-stream',
-    'Content-Length': Buffer.byteLength(postData)
-  }
-}
-
-const req = http.request(options, res => {
-  console.log(`STATUS ${res.statusCodd}`)
-  console.log(`HEADERS: ${JSON.stringify(res.headers)}`)
-  // res.setEncoding('utf8')
-  // res.on('data', chunk => {
-  //   console.log(`BODY: ${chunk}`)
-  // })
-  // res.on('end', () => {
-  //   console.log('No more data in response')
-  // })
-})
-req.on('error', err => {
-  console.error(`problem with request: ${err.message}`)
-})
-
-req.write(postData)
-req.end()
